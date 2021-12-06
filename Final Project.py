af17ec068f1a2b26c6543c15d4610b5606f3a775
whoisdenny77 <denny77@uchicago.edu>
2021-12-06 04:34:53 -0800

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -12,6 +12,10 @@ import seaborn as sns
 from sklearn import linear_model
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
 cwd = os.getcwd()
 path =r'C:\Users\xiaor\Documents\GitHub\Final-Project'
 
@@ -59,7 +63,7 @@ df = df.replace({'fte_grade':fte_dict}) #five thirty eight column replaced with 
 df_democrat = df[df['candidate_party'] == 'DEM']
 df_republican = df[df['candidate_party'] == 'REP']
 ####
-# Regression
+# Linear Regression
 ####
 
 X = df_democrat.iloc[:, 16].values.reshape(-1, 1) #percentage index starting from 0
@@ -67,6 +71,86 @@ Y = df_democrat.iloc[:, 19].values.reshape(-1, 1) #yes approval for biden
 linear_regressor = LinearRegression()
 linear_regressor.fit(X, Y)
 Y_pred = linear_regressor.predict(X)
+
+model =sm.OLS(Y, X).fit()
+print_model = model.summary()
+print(print_model)     #displaying OLS model summary p values for democrats
+
+####  Multi-Linear Regression
+
+X2 = df_democrat[['pct','sample_size_president','fte_grade','sample_size_governor']]
+Y2 = df_democrat['no']
+
+regr = linear_model.LinearRegression()
+regr.fit(X2, Y2)
+
+model2 =sm.OLS(Y2, X2).fit()
+print_model2 = model2.summary()
+print(print_model2)   #insignificant pct result correlation
+
+
+####
+# Machine Learning Supervised:Classfication
+####
+
+#split exo X and endo Y into training and testing sets
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.3, random_state= 66)
+#linear regression object 
+regres = linear_model.LinearRegression()
+
+#train the model
+regres.fit(X_train, Y_train)
+
+#score from training
+print(regres.score(X_train, Y_train))
+
+#score from testing
+print(regres.score(X_test,Y_test))
+
+#trying some GradientBoosteringRegressor Model for better score
+
+regres2 = GradientBoostingRegressor(random_state=66).fit(X_train, Y_train)
+
+#score from training
+print(regres2.score(X_train, Y_train))
+
+#score from testing
+print(regres2.score(X_test,Y_test))
+
+#trying RandomForestRegressor Model for better score too
+regres3 = RandomForestRegressor(random_state=66).fit(X_train, Y_train)
+
+#score from training
+print(regres3.score(X_train, Y_train))
+
+#score from testing
+print(regres3.score(X_test,Y_test))
+
+###
+### RandomForestRegresor test yields the highest Determination of Coefficent
+### As known as the R-square. We look at the test score b/c its unbiased
+###
+#traina and plot
+plt.style.use('fivethirtyeight')  #since data source from 538, use its style
+
+## plotting residual errors in training data
+plt.scatter(regres.predict(X_train), regres.predict(X_train) - Y_train,
+            color = "green", s = 8, label = 'Train data')
+ 
+## plotting residual errors in test data
+plt.scatter(regres.predict(X_test), regres.predict(X_test) - Y_test,
+            color = "blue", s = 8, label = 'Test data')
+ 
+## plotting line for zero residual error
+plt.hlines(y = 0, xmin = 0, xmax = 60, linewidth = 2)
+ 
+## plotting legend
+plt.legend(loc = 'best')
+ 
+## plot title
+plt.title("Residual Errors for Democrats Governor Poll vs Biden Approval Rate")
+plt.show()
+
 
 ####
 # Plotting
@@ -77,4 +161,16 @@ plt.show()
 
 # OR if revealing in Seaborn
 sns.lmplot(x='pct',y='yes',data=df_democrat,fit_reg=True) #democrat
-sns.lmplot(x='pct',y='no',data=df_republican,fit_reg=True) #republican
+sns.lmplot(x='pct',y='yes',data=df_republican,fit_reg=True) #republican
+
+g =sns.lmplot(x="pct", y="yes", col="candidate_party", hue="candidate_party",
+               data=df, col_wrap=2, height=4)
+g.set(xlim=(0,70),ylim =())
+
+####
+# Interactive Plotting
+####
+
+
+
+
