ca29af0481471264a19957228a806015ca9863c9
whoisdenny77 <denny77@uchicago.edu>
2021-12-06 01:47:52 -0800

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -7,16 +7,27 @@ Created on Mon Nov 15 16:27:46 2021
 
 import pandas as pd
 import os
-
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+from sklearn import linear_model
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
 cwd = os.getcwd()
+path =r'C:\Users\xiaor\Documents\GitHub\Final-Project'
+
 fname1 = 'governor_polls.csv'
 fname2 = 'president_approval_polls.csv'
 
-governor = pd.read_csv(os.path.join(cwd, fname1))
-president = pd.read_csv(os.path.join(cwd,fname2))
+governor = pd.read_csv(os.path.join(path, fname1))
+president = pd.read_csv(os.path.join(path,fname2))
 
 col1 = governor.columns #show numbers of columns for governor
 col2 = president.columns #show columns of president
+
+
+####
+# Data Cleaning
+####
 
 #since only interested in swing state supporting rate, begin data cleaning
 # by dropping other states
@@ -37,8 +48,33 @@ df_psd = president[select_list]
 
 df = pd.merge(df_gov, df_psd, how='right', on = ['pollster_id','pollster','display_name','pollster_rating_id','pollster_rating_name','fte_grade','methodology'])
 df = df.drop(df.columns[12:15], axis = 1)  #drop three unnessary columns
-df = df.drop('politician_id', axis =1)
+df = df.drop(['politician_id','candidate_id'], axis =1) #some more
 
 # renaming size and candidate columns to clarify between presidents and governor candidates
-#df.rename = 
+df =df.rename(columns = {'sample_size_x':'sample_size_governor','sample_size_y':'sample_size_president','politician':'president'})
+# replacing column fte_grading with numerical value for better analyze
+fte_dict = {'A+':4.3,'A':4.0,'A-':3.7,'B+':3.3,'B':3.0,'B-':2.7,'C+':2.3,'C':2.0,'C-':1.7,'D+':1.3,'D':1.0,'A/B':3.5,'B/C':2.5,'C/D':1.5,}
+df = df.replace({'fte_grade':fte_dict}) #five thirty eight column replaced with numerical, referenced from UChicago GPA agenda
 
+df_democrat = df[df['candidate_party'] == 'DEM']
+df_republican = df[df['candidate_party'] == 'REP']
+####
+# Regression
+####
+
+X = df_democrat.iloc[:, 16].values.reshape(-1, 1) #percentage index starting from 0
+Y = df_democrat.iloc[:, 19].values.reshape(-1, 1) #yes approval for biden
+linear_regressor = LinearRegression()
+linear_regressor.fit(X, Y)
+Y_pred = linear_regressor.predict(X)
+
+####
+# Plotting
+####
+plt.scatter(X, Y)
+plt.plot(X, Y_pred, color='red')
+plt.show()
+
+# OR if revealing in Seaborn
+sns.lmplot(x='pct',y='yes',data=df_democrat,fit_reg=True) #democrat
+sns.lmplot(x='pct',y='no',data=df_republican,fit_reg=True) #republican
